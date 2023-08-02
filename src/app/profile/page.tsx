3cbcdc307c0ae17d3564fa8c4d18957f4ed7f734
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-02 13:00:29 +0530

    Implement logout functionality

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+'use client';
+
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-hot-toast';
 
 const ProfilePage = () => {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get('/api/users/logout');
+      toast.success('Logout successful');
+      router.push('/login');
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
   return (
-    <div>
+    <div className="flex min-h-screen flex-col justify-center items-center gap-3">
       <h1>Profile</h1>
       <hr />
       <p>Profile Page</p>
+      <button
+        type="button"
+        className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+        onClick={logout}
+      >
+        Logout
+      </button>
     </div>
   );
 };
